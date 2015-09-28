8057ad6acde8e51b060f4e1b4ea91c7beb38264a
christian lei & abdul rahimi <student@epicodus.com>
2015-09-28 16:04:21 -0700

    add individual subcategory page

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  displayAdminTile: false,
+  actions: {
+    openAdminTile() {
+      this.set('displayAdminTile', true);
+    },
+    closeAdminTile() {
+      this.set('displayAdminTile', false);
+  },
+
+    saveSubCategory(params) {
+      this.sendAction('saveSubCategory', params)
+    }
+  }
 });
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('subcategory', {path: 'subcategory/:subcategory_id'});
 });
 
 export default Router;
